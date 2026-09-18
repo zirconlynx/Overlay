d35187dceecb05bd4044a60de093fbffbf16061b
zirconlynx <nyctophobic437@gmail.com>
2026-09-18 14:14:52 -0500

    Update preload.js

--- APP/preload.js
+++ APP/preload.js
@@ -1,1 +1,6 @@
+const { contextBridge, ipcRenderer } = require('electron');
 
+contextBridge.exposeInMainWorld('overlayAPI', {
+  triggerAction: (name) => ipcRenderer.send('overlay-action', name),
+  setClickThrough: (enabled) => ipcRenderer.send('overlay-set-click-through', enabled)
+});
